0c20758507bb3b3e434c5053fa39224b2a483022
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-20 14:46:07 -0800

    some code cleanup for readablility

--- Develop/script.js
+++ Develop/script.js
@@ -33,26 +33,16 @@ $(function () {
   $(".time-block").each(function()
   {
     var hour = this.id.replace("hour-", "");
-
-    if (hour == currentHour)
-    {
-      this.classList.add("present");
-    }
-    else if (hour <= currentHour)
-    {
-      this.classList.add("past");
-    }
-    else if (hour >= currentHour)
-    {
-      this.classList.add("future");
-    }
+    if (hour == currentHour) { this.classList.add("present"); }
+    else if (hour <= currentHour) { this.classList.add("past"); }
+    else if (hour >= currentHour) { this.classList.add("future"); }
   });
-  
+
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
 
 
   // TODO: Add code to display the current date in the header of the page.
