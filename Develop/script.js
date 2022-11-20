6b5a5dc72c145e05834ea7a003b7e5e84e26c4cf
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-20 14:43:21 -0800

    input inside time-blocks is now saveable

--- Develop/script.js
+++ Develop/script.js
@@ -5,13 +5,24 @@ $(function () {
   const currentDate = dayjs().format('dddd, MMM D');
   const currentHour = dayjs().format('H');
 
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  const timeBlockArray = ["hour-09", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
+  for (var i = 0; i < 8; i++)
+  {
+    document.getElementById(timeBlockArray[i]).addEventListener('click', storeData.bind(this, timeBlockArray[i], document.getElementById(timeBlockArray[i]).children[1].value));
+  }
+
+  function storeData(timeBlockId, text)
+  {
+    localStorage.setItem(timeBlockId, text);
+    console.log("saved " + localStorage.getItem(timeBlockId) + " to " + timeBlockId + " block data");
+  }
 
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -36,6 +47,7 @@ $(function () {
       this.classList.add("future");
     }
   });
+  
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
